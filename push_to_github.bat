68280d94009a2b59b4c9f30cfe4b0471d400ee4f
KamleshStable <kamlesh@stableonesoftware.com>
2025-05-13 18:49:35 +0530

    Add comprehensive Java interview questions and project files

--- push_to_github.bat
+++ push_to_github.bat
@@ -3,8 +3,7 @@ echo Pushing all Java interview project files to GitHub...
 
 git add .
 git commit -m "Add comprehensive Java interview questions and project files"
-git remote add origin https://github.com/kamkode/java-system-design-prep.git
-git push -u origin main
+git push -u origin master
 
 echo Done! Check your GitHub repository.
 pause
